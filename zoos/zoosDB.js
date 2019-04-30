4796d9b5dd45d09742a7264e6ffd73d404598ffa
Henry Blevins <heblevi@gmail.com>
2019-04-30 16:13:32 -0400

    Implement get route

---
+++ zoos/zoosDB.js
@@ -0,0 +1,9 @@
+const db = require('../data/db.js');
+
+module.exports = {
+  get
+};
+
+function get() {
+  return db('zoos');
+};
--- zoos/zoosRoutes.js
+++ zoos/zoosRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const db = require('./zoosDB.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json("zoos");
+  db.get()
+    .then(zoos => res.status(200).json(zoos))
+    .catch(_err => res.status(500).json({message: "Error fetching zoos."}));
 });
 
 module.exports = router;
