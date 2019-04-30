398a6adac240f01dc5839ff9f351e5648b45f968
Henry Blevins <heblevi@gmail.com>
2019-04-30 16:20:57 -0400

    Implement zoo post route

--- zoos/zoosDB.js
+++ zoos/zoosDB.js
@@ -1,9 +1,19 @@
 const db = require('../data/db.js');
 
 module.exports = {
-  get
+  get,
+  getById,
+  insert
 };
 
 function get() {
   return db('zoos');
+};
+
+function getById(id) {
+  return db('zoos').select().where({id}).first();
+};
+
+function insert(zoo) {
+  return db('zoos').insert(zoo, 'id').then(([id]) => getById(id));
 };
--- zoos/zoosRoutes.js
+++ zoos/zoosRoutes.js
@@ -9,6 +9,17 @@ router.get('/', (req, res) => {
     .catch(_err => res.status(500).json({message: "Error fetching zoos."}));
 });
 
+router.post('/', (req, res) => {
+  const zoo = req.body;
+  if (zoo.name && zoo.name !== '') {
+    db.insert(zoo)
+      .then(zoo => res.status(201).json(zoo))
+      .catch(error => res.status(500).json({message: "Error creating zoo.", error}));
+  } else {
+    res.status(400).json({message: "Zoo requires name."});
+  }
+});
+
 module.exports = router;
 
 
